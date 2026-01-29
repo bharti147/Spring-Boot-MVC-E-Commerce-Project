d3022b5f8d6c5ca0721541e396546bab43879932
Bharti <sahanibharti71@gmail.com>
2026-01-29 22:42:10 +0530

    models order and orderItem done

--- src/main/java/com/sb/springboot_mvc_ecommerce/model/Order.java
+++ src/main/java/com/sb/springboot_mvc_ecommerce/model/Order.java
@@ -1,13 +1,33 @@
 package com.sb.springboot_mvc_ecommerce.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+
+@Entity(name = "orders")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Order {
+    @Column(unique = true)
+    private String orderId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String customerName;
+    private String email;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items;
+    private String status;
+    private LocalDate orderDate;
+
+
+
 }
--- src/main/java/com/sb/springboot_mvc_ecommerce/model/OrderItem.java
+++ src/main/java/com/sb/springboot_mvc_ecommerce/model/OrderItem.java
@@ -1,13 +1,27 @@
 package com.sb.springboot_mvc_ecommerce.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    private Product product;
+    private BigDecimal totalPrice;
+    private int quantity;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
 }
